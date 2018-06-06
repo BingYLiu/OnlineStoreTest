23b03928dad17dd6ddd2adf43bb86368780af358
BingYLiu <jimmyliu0131@gmail.com>
2018-06-06 08:29:31 -0400

    update testscript

--- script/runTest.bat
+++ script/runTest.bat
@@ -1,3 +1,3 @@
-set CLASSPATH=../lib/*;../bin/*
+set CLASSPATH=../lib/*;../bin/OnlineStoreAutomation/*;../bin/SeleniumFixture/*
 set PROJECTPATH= ../
 java -cp %CLASSPATH% org.testng.TestNG %PROJECTPATH%testng.xml
