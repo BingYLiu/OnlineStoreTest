05fce6453b286f6295b450c0dc611c9a20a8424e
BingYLiu <jimmyliu0131@gmail.com>
2018-06-07 19:52:55 -0400

    remove useless import

--- src/junkinLazyScript/InstallJenkinForLazy.java
+++ src/junkinLazyScript/InstallJenkinForLazy.java
@@ -3,12 +3,9 @@ package junkinLazyScript;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
