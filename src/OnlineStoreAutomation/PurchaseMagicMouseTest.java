d2cb6325341e1f8f04e599ee973ea29b0207be8b
BingYLiu <jimmyliu0131@gmail.com>
2018-06-06 22:59:26 -0400

    update gecodriver

--- src/OnlineStoreAutomation/PurchaseMagicMouseTest.java
+++ src/OnlineStoreAutomation/PurchaseMagicMouseTest.java
@@ -13,19 +13,22 @@ import org.testng.annotations.*;
 public class PurchaseMagicMouseTest {
 	private WebDriver driver;
 
-
-	
 	@DataProvider(name = "DummyData")
-	public static Object[][] userInfo(){
-		return new Object[][] {
-			{"Dummy.Deadbeef@Dummy.com","DUMMY","DEADBEEF","4 Privet Drive","Toronto","Ontario","Canada","M3M 1C1","3735928559"}
-		};
+	public static Object[][] userInfo() {
+		return new Object[][] { { "Dummy.Deadbeef@Dummy.com", "DUMMY", "DEADBEEF", "4 Privet Drive", "Toronto",
+				"Ontario", "Canada", "M3M 1C1", "3735928559" } };
 	}
-	
+
 	@BeforeSuite
 	public void beforeSuiteSetup() {
 		System.out.println("Configure Suite");
-		System.setProperty("webdriver.gecko.driver", "Tools\\geckodriver\\geckodriver.exe");
+		String os = System.getProperty("os.name").toLowerCase();
+		if (os.indexOf("win") >= 0) {
+			System.setProperty("webdriver.gecko.driver", "Tools\\geckodriver\\geckodriver.exe");
+		}else if(os.indexOf("mac") >= 0)
+		{
+			System.setProperty("webdriver.gecko.driver", "Tools\\geckodriver\\geckodriver");
+		}
 	}
 
 	@BeforeTest
@@ -39,38 +42,39 @@ public class PurchaseMagicMouseTest {
 	public void afterTest() {
 		this.driver.quit();
 	}
-	
-	@Test(description = "Varify I can Purchase a magic mouse with valid user infos",dataProvider = "DummyData")
-	public void ContinueCheckout(String email,String firstName,String lastName,String address,String city, String province, String country, String postCode, String phone) {
 
-		//Ensure Home Page is displayed
+	@Test(description = "Varify I can Purchase a magic mouse with valid user infos", dataProvider = "DummyData")
+	public void ContinueCheckout(String email, String firstName, String lastName, String address, String city,
+			String province, String country, String postCode, String phone) {
+
+		// Ensure Home Page is displayed
 		new HomePage(driver).get();
-		assertTrue(driver.getTitle().contains("ONLINE STORE"),"Unable to Load Home Page");
-		
-		
-		//Ensure Header and Footer is displayed and I can Navigate to Accessories Page Via Menu
+		assertTrue(driver.getTitle().contains("ONLINE STORE"), "Unable to Load Home Page");
+
+		// Ensure Header and Footer is displayed and I can Navigate to Accessories Page
+		// Via Menu
 		HeaderFooterPage hf = new HeaderFooterPage(driver).get();
 		hf.selectDropdownMenuItem("Product Category", "Accessories");
-		assertTrue(driver.getTitle().contains("Accessories"),"Unable to Navigate to Accessories");
-		
-		//Ensure I can add magic mouse to cart
+		assertTrue(driver.getTitle().contains("Accessories"), "Unable to Navigate to Accessories");
+
+		// Ensure I can add magic mouse to cart
 		ProductPage ap = new ProductPage(driver).get();
 		ap.clickAddToCart("Magic Mouse");
-		assertTrue(hf.getCartText().contains("1 item"),"Item in cart is incorrect");
+		assertTrue(hf.getCartText().contains("1 item"), "Item in cart is incorrect");
 
-		//Ensure I can move to checkout page
+		// Ensure I can move to checkout page
 		hf.clickCheckout();
-		assertTrue(driver.getTitle().contains("Checkout"),"Unable to Navigate to Checkout");
-		
-		//Ensure Checkout Page is correct
+		assertTrue(driver.getTitle().contains("Checkout"), "Unable to Navigate to Checkout");
+
+		// Ensure Checkout Page is correct
 		CheckoutPage cp = new CheckoutPage(driver).get();
-		assertTrue(cp.getQuantity("Magic Mouse") == 1,"Item quantity is incorrect in Checkout");
-		
-		//Ensure I can continue to billing info page
+		assertTrue(cp.getQuantity("Magic Mouse") == 1, "Item quantity is incorrect in Checkout");
+
+		// Ensure I can continue to billing info page
 		cp.clickContinueButton();
-		assertTrue(cp.isCheckOutFormsDisplayed(),"user form is not displayed");
-		
-		//Input user info to checkout form
+		assertTrue(cp.isCheckOutFormsDisplayed(), "user form is not displayed");
+
+		// Input user info to checkout form
 		cp.enterValueToTextBox("email", email, "Billing");
 		cp.enterValueToTextBox("firstName", firstName, "Billing");
 		cp.enterValueToTextBox("lastName", lastName, "Billing");
@@ -81,10 +85,11 @@ public class PurchaseMagicMouseTest {
 		cp.selectDropDownText("country", country, "Billing");
 		// cp.selectDropDownText("region", province,"Billing");
 
-		//Ensure I can finish purchase with correct items
+		// Ensure I can finish purchase with correct items
 		cp.clickPurchaseButton();
 		TransactionResultsPage trp = new TransactionResultsPage(driver);
-		assertTrue(trp.getColumnValueForItem("Quantity", "Magic Mouse").equals("1"),"Transaction Result is not correct");
+		assertTrue(trp.getColumnValueForItem("Quantity", "Magic Mouse").equals("1"),
+				"Transaction Result is not correct");
 	}
-	
+
 }
